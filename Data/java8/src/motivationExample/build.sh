00d924aa8c02de18a7c68ef583c13b4e507c0973
Taeyong Seong <thebigmercury@gmail.com>
2023-07-04 18:59:31 +0900

    shスクリプトのリファクタリング

--- Data/java8/src/motivationExample/build.sh
+++ Data/java8/src/motivationExample/build.sh
@@ -1,47 +1,29 @@
+set -e
+
+compile_and_run() {
+    local directory=$1
+    local class_name=$2
+
+    echo compile "$class_name"
+    cd "$directory"
+    rm *.class || true
+    javac -g "$class_name.java"
+    cd ../
+
+    echo compile Main
+    rm *.class || true
+    javac -g Main.java
+
+    echo run selogger with omni
+    java  -javaagent:../../../selogger/target/selogger-0.5.1.jar=output=selogger_out,format=omni,waverlog=hogehoeg_log.txt Main
+}
 
 # create log dir for origin
 cd origin/
 
-echo compile customClass
-cd customClass/
-rm *.class
-javac -g myMath.java 
-
-cd ../
-echo compile Main
-rm *.class
-javac -g Main.java
-
-echo run selogger with omni
-java  -javaagent:../../../selogger/target/selogger-0.5.0.jar=output=selogger_out,format=omni,waverlog=hogehoeg_log.txt Main
+compile_and_run customClass myMath
 
 #======================== target ================================
 cd ../target
-echo compile customClass
-cd customClass/
-rm *.class
-javac -g myMath.java 
 
-cd ../
-echo compile Main
-rm *.class
-javac -g Main.java
-
-echo run selogger with omni
-java  -javaagent:../../../selogger/target/selogger-0.5.0.jar=output=selogger_out,format=omni,waverlog=hogehoeg_log.txt Main
-
-
-
-# echo run selogger with omnibinary
-# java -javaagent:../../../selogger/target/selogger-0.5.0.jar=output=selogger_bin_out,format=omnibinary, Main
-
-# echo run selogger.reader
-# java -classpath ../../../selogger/target/selogger-0.5.0.jar selogger.reader.LogPrinter 
-
-# create Log dir for target
-# cd ../target
-# javac Main.java
-# echo run selogger with omni
-# java  -javaagent:../../../selogger/target/selogger-0.5.0.jar=output=selogger_out,format=omni,waverlog=hogehoeg_log.txt Main
-
-# rm Main.class
+compile_and_run customClass myMath

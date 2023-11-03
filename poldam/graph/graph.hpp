2963987fe57ae73e6e2335f15edc8036c9bc2041
T.S <thebigmercury@gmail.com>
2023-11-04 03:14:42 +0900

    graphを新しいディレクトリへと移動

---
+++ poldam/graph/graph.hpp
@@ -0,0 +1,210 @@
+#pragma once 
+#include <poldam/util/utility.hpp>
+#include <poldam/util/arg_config.hpp>
+
+#include <boost/graph/adjacency_iterator.hpp>
+#include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/connected_components.hpp>
+
+namespace POLDAM
+{
+    struct GraphEdge
+    {
+    public:
+        int cost;
+        std::string edgeHash;
+        std::string from;
+        std::string to;
+        std::string outputFormat;
+    };
+
+    struct GraphVertex
+    {
+    public:
+        std::string classStr{};
+        // method hash is the hash of method
+        std::string methodStr{};
+        std::string methodHash{};
+        // param str has param value, the hash value of object
+        std::string paramStr{};
+        std::size_t paramHash{};
+        // flow hash is the flow of inside of the method
+        std::string flowStr{};
+        std::size_t flowHash{};
+        // control flow hash is the method-call-chain of the hash
+        std::size_t childFlowHash{};
+        std::size_t childParamHash{};
+        std::string controlFlowStr{};
+        std::size_t controlFlowHash{};
+        std::string controlParamStr{};
+        std::size_t controlParamHash{};
+        /**
+         *  For the graph view
+         */
+        bool isTargetVertex = false;
+
+        /*
+         * context hash is the flow hash of the parent hash.
+         */
+        std::size_t contextHash{};
+        std::string outputFormat{};
+        size_t id{}, edgesSize{};
+        std::vector<std::string> paramList = {};
+    };
+
+    using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                                        GraphVertex,
+                                        GraphEdge>;
+    struct VertexPredicate
+    {
+        VertexPredicate(){};
+        VertexPredicate(Graph const *g) : _graph(g){};
+        bool operator()(Graph::vertex_descriptor const &v) const
+        {
+            return (*_graph)[v].isTargetVertex;
+        }
+
+        Graph const *_graph;
+    };
+
+    class PoldamGraph
+    {
+    public:
+        POLDAM::poldamConfig config;
+        // constructor
+        PoldamGraph(const poldamConfig c) : config(c){};
+        PoldamGraph() : config(poldamConfig{}){};
+        /**
+         * @brief Create vertex Descriptor from given Graph Vertex, and Create edge if vStack is not empty.\
+         *
+         * @param v GraphVertex,
+         * @param threadId
+         * @return true  return true if succedd to add vertex to graph.
+         * @return false return false if failed to add vertex to graph.
+         */
+        bool addOmniVertex(GraphVertex v, const size_t threadId);
+
+        /**
+         * @brief
+         *
+         * @param log string, add log context to vStack top vertex.
+         * @param threadId
+         * @return true
+         * @return false
+         */
+        bool updateStackTopVertex(const std::string log, const unsigned int threadId);
+
+        bool updateStackTopVertexParamInfo(const std::string &paramValue, unsigned int threadId);
+        bool popStackVertex(const unsigned int threadId);
+
+        bool computeHash(const unsigned int threadId);
+
+        bool isStackEmpty(const unsigned int threadId);
+
+        Graph getGraphCopy();
+
+        boost::graph_traits<Graph>::vertex_descriptor getStackTopVertex(const unsigned int threadId);
+
+        std::vector<boost::graph_traits<Graph>::vertex_descriptor> copyGraphPath()
+        {
+            return this->path;
+        };
+
+        const boost::graph_traits<Graph>::vertex_descriptor getRoot()
+        {
+            // throw exception if Root has not been initialized.
+            assert(this->root.size() > 0);
+            return this->Root;
+        }
+
+        PoldamGraph computeDiffGraph(PoldamGraph &targetGraph);
+        // need to move Graph
+        Graph computeDiffGraphBeta(PoldamGraph targetGraph, const std::function<bool(const GraphVertex &, const GraphVertex &)> &isSameVertex);
+
+    private:
+        bool computeFlowHash(const unsigned int threadId);
+        bool computeParamHash(const unsigned int threadId);
+        /**
+         * @brief the implementation when vertex Stack is popped. the function describe the relationship between caller and callee.
+         *
+         * @param threadId the threadId of target log
+         * @return true implementation has succesfully finished.
+         * @return false implementation has not finished succesfully.
+         */
+        bool popVertex(const unsigned int threadId);
+
+        /**
+         * @brief return true if given vertex descriptor is leaf in graph.
+         *
+         * @param vDesc boost::graph_traits<Graph>::vertex_descriptor
+         * @return true
+         * @return false
+         */
+        bool isLeaf(boost::graph_traits<Graph>::vertex_descriptor vDesc);
+
+        /**
+         * @brief return true if given vertex descriptor is leaf in graph.
+         *
+         * @param vDesc boost::graph_traits<Graph>::vertex_descriptor
+         * @return true
+         * @return false
+         */
+        bool pushStackVertex(const boost::graph_traits<Graph>::vertex_descriptor vDesc, const unsigned int threadId);
+
+        /**
+         * @brief Create VertexDescriptor. Note this function DO NOT add to graph.\
+         * NOTE: this function do not update vStack, Therefore, you need to handle it carefully.
+         *
+         * @param v
+         * @param threadId
+         * @return boost::graph_traits<Graph>::vertex_descriptor return vertex_descriptor if succedd to add
+         */
+        boost::graph_traits<Graph>::vertex_descriptor addOmniVertexDesc(GraphVertex v, const size_t threadId);
+
+        /**
+         * @brief Add edge between two given vertices.
+         *
+         * @param u_
+         * @param v_
+         * @param threadId
+         * @return true
+         * @return false
+         */
+        bool addOmniEdge(
+            boost::graph_traits<Graph>::vertex_descriptor u_,
+            boost::graph_traits<Graph>::vertex_descriptor v_, const size_t threadId);
+
+        void traverseDiffVertices(
+            const Graph &o, const Graph &t,
+            const boost::graph_traits<Graph>::vertex_descriptor ovd,
+            const boost::graph_traits<Graph>::vertex_descriptor tvd,
+            const boost::graph_traits<Graph>::vertex_descriptor parDiffVerDesc,
+            const std::function<bool(const GraphVertex &, const GraphVertex &)> &isSameVertex
+
+        );
+
+        unsigned int incrementCounter()
+        {
+            return this->counter++;
+        };
+
+        Graph g{};
+        Graph diffGraph{};
+        unsigned int counter = 0;
+
+        boost::graph_traits<Graph>::vertex_descriptor Root;
+
+        std::map<unsigned int, boost::graph_traits<Graph>::vertex_descriptor> root{};
+
+        std::vector<boost::graph_traits<Graph>::vertex_descriptor> path;
+        // stack for vertexies
+
+        // {key: thareadId, value:stack<>}
+        std::map<unsigned int, std::stack<boost::graph_traits<Graph>::vertex_descriptor>> vStack;
+        // {key: threadId, value:stack<>}
+        std::map<unsigned int, std::stack<std::map<std::string, std::string>>> callStack;
+        // {key: threadId, value:stack<>}
+        std::map<unsigned int, std::stack<std::map<std::string, std::string>>> callInstStack;
+    };
+}
---
+++ poldam/graph/graph_diff.cpp
@@ -0,0 +1,304 @@
+#include <poldam/graph/graph.hpp>
+
+/**
+ * GraphDiff algorithm is beta, There might be good way to compute the diff between two graphs, like following URL
+ * https://www.researchgate.net/publication/220618233_Simple_Fast_Algorithms_for_the_Editing_Distance_Between_Trees_and_Related_Problems
+ *
+ */
+namespace POLDAM
+{
+    bool isSameVertex(GraphVertex v, GraphVertex u)
+    {
+        return v.controlFlowHash == u.controlFlowHash;
+    }
+
+    /**
+     * @brief **DEPRECATED**\a
+     * Diff algorithm to compute the difference between origin and target
+     * @param target
+     * @return PoldamGraph the diffGraph
+     */
+    PoldamGraph PoldamGraph::computeDiffGraph(PoldamGraph &target)
+    {
+        PoldamGraph diffGraph;
+
+        const Graph originGraph = this->g;
+        const Graph targetGraph = target.getGraphCopy();
+
+        std::vector<boost::graph_traits<Graph>::vertex_descriptor> originPath, targetPath;
+
+        originPath = this->path;
+        targetPath = target.copyGraphPath();
+
+        unsigned int originDescIdx = 0, targetDescIdx = 0;
+
+        // For the diffgraph, you do not need to desginate threadId
+        const unsigned int defaultThreadId = 0;
+
+        while (originDescIdx < originPath.size() && targetDescIdx < targetPath.size())
+        {
+            boost::graph_traits<Graph>::vertex_descriptor originDesc, targetDesc;
+
+            originDesc = originPath[originDescIdx];
+            targetDesc = targetPath[targetDescIdx];
+
+            GraphVertex originVertex = originGraph[originDesc];
+            GraphVertex targetVertex = targetGraph[targetDesc];
+
+            if (isSameVertex(originVertex, targetVertex))
+            {
+                ++originDescIdx;
+                ++targetDescIdx;
+            }
+            else
+            { // Different flow is detected.
+                boost::graph_traits<Graph>::vertex_descriptor prevVertexDesc = -1;
+
+                if (not diffGraph.isStackEmpty(defaultThreadId))
+                {
+                    prevVertexDesc = diffGraph.getStackTopVertex(defaultThreadId);
+                }
+
+                if (originVertex.methodHash == targetVertex.methodHash)
+                {
+                    // Different Execution is detected in same method.
+                    // Proceed Diff algorithm until a differnt method call is detected or method on the execution path becomes a leaf in this graph.
+                    boost::graph_traits<Graph>::vertex_descriptor diffOriginVertexDesc = diffGraph.addOmniVertexDesc(originVertex, defaultThreadId);
+
+                    // if diffOriginVertexDesc is root vertex, no need to create edge.
+                    if (diffOriginVertexDesc != diffGraph.root[defaultThreadId])
+                    {
+                        assert(prevVertexDesc != -1);
+                        diffGraph.addOmniEdge(prevVertexDesc, diffOriginVertexDesc, defaultThreadId);
+                    }
+                    else
+                    {
+                        diffGraph.pushStackVertex(diffOriginVertexDesc, defaultThreadId);
+                    }
+                    ++originDescIdx;
+                    ++targetDescIdx;
+
+                    // if the diff between two graph is too big, should use early return.
+                    if (PoldamGraph::isLeaf(originDesc))
+                    {
+                        return diffGraph;
+                    }
+                }
+                else /*if originVertex.methodHash != targetVertex.methodHash : DIFFERENT METHOD IS CALLED*/
+                {
+                    /**
+                     *  if different method call is detected, it's impossible to compare the two method callds
+                     */
+
+                    boost::graph_traits<Graph>::vertex_descriptor diffOriginVertexDesc = diffGraph.addOmniVertexDesc(originVertex, defaultThreadId);
+                    boost::graph_traits<Graph>::vertex_descriptor diffTargetVertexDesc = diffGraph.addOmniVertexDesc(targetVertex, defaultThreadId);
+
+                    if (diffOriginVertexDesc != diffGraph.root[defaultThreadId])
+                    {
+                        assert(prevVertexDesc != -1);
+                        diffGraph.addOmniEdge(prevVertexDesc, diffOriginVertexDesc, defaultThreadId);
+                        diffGraph.addOmniEdge(prevVertexDesc, diffTargetVertexDesc, defaultThreadId);
+                    }
+                    else
+                    {
+                        // In this case, the root vertices are different
+                        return POLDAM::PoldamGraph();
+                    }
+
+                    // try to append vertex until its leaf
+                    originDescIdx++;
+
+                    boost::graph_traits<Graph>::vertex_descriptor branchDesc{};
+                    GraphVertex branchVertex{};
+                    std::cout << "never reach here\n";
+                    while (originDescIdx < originPath.size() && !isLeaf(originDescIdx))
+                    {
+                        // copy from branch vertex Info
+                        branchDesc = originPath[originDescIdx];
+                        branchVertex = originGraph[branchDesc];
+
+                        boost::graph_traits<Graph>::vertex_descriptor newDiffVertexDesc = diffGraph.addOmniVertexDesc(branchVertex, defaultThreadId);
+
+                        diffGraph.addOmniEdge(diffOriginVertexDesc, newDiffVertexDesc, defaultThreadId);
+
+                        diffOriginVertexDesc = newDiffVertexDesc;
+                        originDescIdx++;
+                    }
+
+                    while (targetDescIdx < targetPath.size() && !isLeaf(targetDescIdx))
+                    {
+                        // copy from branch vertex Info
+                        branchDesc = targetPath[targetDescIdx];
+                        branchVertex = targetGraph[branchDesc];
+
+                        boost::graph_traits<Graph>::vertex_descriptor newDiffVertexDesc = diffGraph.addOmniVertexDesc(branchVertex, defaultThreadId);
+
+                        diffGraph.addOmniEdge(diffTargetVertexDesc, newDiffVertexDesc, defaultThreadId);
+
+                        diffOriginVertexDesc = newDiffVertexDesc;
+                        targetDescIdx++;
+                    }
+
+                    return diffGraph;
+                }
+            }
+        }
+
+        if (originDescIdx >= originPath.size() && targetDescIdx >= targetPath.size())
+        {
+            // Itereate ALL vertices , return Immediately
+            return diffGraph;
+        }
+        else if (originDescIdx >= originPath.size())
+        {
+        }
+        else if (targetDescIdx >= targetPath.size())
+        {
+        }
+
+        return diffGraph;
+    }
+
+    /**
+     * @brief  traverse tree to compute diff algorithm
+     * @param originGraph
+     * @param targetGraph
+     * @param originVerDesc
+     * @param targetVerDesc
+     * @param parDiffVerDesc
+     */
+    void PoldamGraph::traverseDiffVertices(const Graph &originGraph, const Graph &targetGraph,
+                                           const boost::graph_traits<Graph>::vertex_descriptor originVerDesc,
+                                           const boost::graph_traits<Graph>::vertex_descriptor targetVerDesc,
+                                           const boost::graph_traits<Graph>::vertex_descriptor parDiffVerDesc,
+                                           const std::function<bool(const GraphVertex &v, const GraphVertex &u)> &isSameVertex)
+    {
+
+        // assert(not isSameVertex(originGraph[originVerDesc], targetGraph[targetVerDesc]));
+
+        boost::graph_traits<Graph>::out_edge_iterator oEdgeItr,
+            oEdgeItrEnd, tEdgeItr, tEdgeItrEnd;
+
+        const unsigned int oOutEdgeSize = boost::out_degree(originVerDesc, originGraph);
+        const unsigned int tOutEdgeSize = boost::out_degree(targetVerDesc, targetGraph);
+
+        // if oOutEdgeSize  != tOutEdgeSize, it indicate there is a different method call.
+
+        if (oOutEdgeSize != tOutEdgeSize)
+        {
+            std::cout << "DIFFERENT NUMBER OF METHOD CALL IS DETECTED\n";
+
+            boost::tie(oEdgeItr, oEdgeItrEnd) = boost::out_edges(originVerDesc, originGraph);
+            boost::tie(tEdgeItr, tEdgeItrEnd) = boost::out_edges(targetVerDesc, targetGraph);
+
+            for (unsigned int i = 0; i < std::min(oOutEdgeSize, tOutEdgeSize); ++i)
+            {
+                const boost::graph_traits<Graph>::vertex_descriptor nOriginVerDesc = boost::target(*oEdgeItr, originGraph);
+                const boost::graph_traits<Graph>::vertex_descriptor nTragetVerDesc = boost::target(*tEdgeItr, targetGraph);
+
+                boost::graph_traits<Graph>::edge_descriptor edgeDesc;
+                boost::graph_traits<Graph>::vertex_descriptor verDesc;
+
+                verDesc = boost::add_vertex(this->diffGraph);
+                diffGraph[verDesc] = originGraph[nOriginVerDesc];
+                bool isInserted = false;
+                boost::tie(edgeDesc, isInserted) = boost::add_edge(parDiffVerDesc, verDesc, this->diffGraph);
+                ++oEdgeItr;
+                ++tEdgeItr;
+            }
+
+            boost::graph_traits<Graph>::vertex_descriptor extraMethodCallDesc = boost::add_vertex(this->diffGraph);
+
+            if (oEdgeItr != oEdgeItrEnd)
+            {
+                this->diffGraph[extraMethodCallDesc] = originGraph[boost::target(*oEdgeItr, originGraph)];
+            }
+            else
+            {
+                assert(tEdgeItr != tEdgeItrEnd);
+
+                this->diffGraph[extraMethodCallDesc] = targetGraph[boost::target(*tEdgeItr, targetGraph)];
+            }
+
+            this->diffGraph[extraMethodCallDesc].outputFormat += "\nDIFFERENT METHOD CALL";
+            boost::graph_traits<Graph>::edge_descriptor edgeDesc;
+            bool isInserted = false;
+            boost::tie(edgeDesc, isInserted) = boost::add_edge(parDiffVerDesc, extraMethodCallDesc, this->diffGraph);
+
+            return;
+        }
+
+        // oOutEdgeSize ==  tOutEdgeSize --> True
+
+        boost::tie(oEdgeItr, oEdgeItrEnd) = boost::out_edges(originVerDesc, originGraph);
+        boost::tie(tEdgeItr, tEdgeItrEnd) = boost::out_edges(targetVerDesc, targetGraph);
+
+        for (unsigned int i = 0; i < oOutEdgeSize; ++i)
+        {
+            if (oEdgeItr != oEdgeItrEnd && tEdgeItr != oEdgeItrEnd)
+            {
+                const boost::graph_traits<Graph>::vertex_descriptor nOriginVerDesc = boost::target(*oEdgeItr, originGraph);
+                const boost::graph_traits<Graph>::vertex_descriptor nTragetVerDesc = boost::target(*tEdgeItr, targetGraph);
+
+                boost::graph_traits<Graph>::edge_descriptor edgeDesc;
+                boost::graph_traits<Graph>::vertex_descriptor verDesc;
+
+                verDesc = boost::add_vertex(this->diffGraph);
+                diffGraph[verDesc] = originGraph[nOriginVerDesc];
+                bool isInserted = false;
+                boost::tie(edgeDesc, isInserted) = boost::add_edge(parDiffVerDesc, verDesc, this->diffGraph);
+
+                if (isSameVertex(originGraph[nOriginVerDesc], targetGraph[nTragetVerDesc]))
+                {
+                    // no need to traverse anymore.
+                    diffGraph[verDesc].outputFormat += "\nSAME_HASH";
+                }
+                else
+                {
+                    diffGraph[verDesc].outputFormat += "\nDIFFERENT_HASH";
+
+                    traverseDiffVertices(originGraph, targetGraph, nOriginVerDesc, nTragetVerDesc, verDesc, isSameVertex);
+                    break;
+                }
+
+                ++oEdgeItr;
+                ++tEdgeItr;
+            }
+        }
+
+        return;
+    }
+
+    /**
+     * @brief compute Diff between given PoldamGraph and this->g through trversal tree.
+     *
+     * @param target
+     * @return Graph
+     */
+    Graph PoldamGraph::computeDiffGraphBeta(PoldamGraph target,
+                                            const std::function<bool(const GraphVertex &v, const GraphVertex &u)> &isSameVertex)
+    {
+        const Graph &originGraph = this->g;
+        const Graph &targetGraph = target.g;
+
+        unsigned int originDescIdx = 0, targetDescIdx = 0;
+        const unsigned int defaultThreadId = 0;
+
+        // create root vertices
+        boost::graph_traits<Graph>::vertex_descriptor diffGraphRootDesc = boost::add_vertex(this->diffGraph);
+
+        this->diffGraph[diffGraphRootDesc].outputFormat = "DIFF_GRAPH_ENTRY";
+        if (this->config.hasEntryMethodName)
+        {
+            this->diffGraph[diffGraphRootDesc].outputFormat += "\n" + this->config.entryMethodName;
+        }
+
+        traverseDiffVertices(originGraph, targetGraph,
+                             this->getRoot(),
+                             target.getRoot(),
+                             diffGraphRootDesc,
+                             isSameVertex);
+
+        return this->diffGraph;
+    }
+}

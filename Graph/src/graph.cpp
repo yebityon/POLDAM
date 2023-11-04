e1277da157c19f6819a074104da2ee686cdcff5d
T.S <thebigmercury@gmail.com>
2023-11-04 13:09:55 +0900

    Feat/weekly update 10 29 (#28)
    * 再帰をrootで止まるように直した
    
    * regexpがマッチする対象をclassStrに変更した

--- Graph/src/graph.cpp
+++ Graph/src/graph.cpp
@@ -23,17 +23,19 @@ namespace POLDAM
                 // EtntryPointを用意してproc{}を呼ぶよう変更する
                 this->Root = v;
                 this->g[v].isTargetVertex = true;
+                this->g[v].isFilreViewRoot = true;
                 this->root[threadId] = v;
             }
             else if( this -> root.find(threadId) != this -> root.end() && config.hasFilterdRegex) 
             {
-                this->g[v].isTargetVertex = std::regex_match(this->g[v].methodStr, config.filterdVertexRegex);
+                this->g[v].isTargetVertex = std::regex_match(this->g[v].classStr, config.filterdVertexRegex);
             }
         }
         else if(this -> root.find(threadId) == this -> root.end())
         {
             this -> g[v].isTargetVertex = true;
             this->Root = v;
+            this->g[v].isFilreViewRoot = true;
             this->root[threadId] = v;
         }
         
@@ -165,7 +167,8 @@ namespace POLDAM
             for (auto it = boost::adjacent_vertices(crtVertex, this->g); it.first != it.second; ++it.first)
             {
                 const auto &childVertex = *it.first;
-                if (this->g[childVertex].isTargetVertex)
+                // 子孫がRootだと対象外なので伝搬をそこでやめる
+                if (this->g[childVertex].isTargetVertex && !this->g[childVertex].isFilreViewRoot)
                 {
                     this -> g[crtVertex].isTargetVertex = true;
                     break;
